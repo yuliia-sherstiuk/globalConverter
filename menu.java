f63a0113289bc121c955aae6446ba3d8c0460a79
yuliia-sherstiuk <yuliia.sherstiuk@laplateforme.io>
2025-06-23 13:22:55 +0200

    Add display main menu

---
+++ menu.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean continueApp = true;
+
+        while (continueApp) {
+            System.out.println("======== Base Converter ========");
+            System.out.println("1. Convert text to base");
+            System.out.println("2. Quit");
+            System.out.println("=====================================");
+            System.out.print("Your choice (1-2): ");
+            String choice = scanner.nextLine();
+
+            
+                 }
+        }
+    }
+}
