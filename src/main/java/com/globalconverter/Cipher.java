da11a83803bc7cc3f6e792dc8338ff81ea1e6f12
christine chemali <christine.chemali@laplateforme.io>
2025-06-23 22:13:07 +0200

    Encrypt decrypt Caesar number

--- src/main/java/com/globalconverter/Cipher.java
+++ src/main/java/com/globalconverter/Cipher.java
@@ -1,5 +1,32 @@
 package com.globalconverter;
 
 public class Cipher {
-    
+
+    //CAESAR CIPHER
+    /*
+     * Encrypt text with caesar number
+     * Take param:
+     * text = text to encrypt
+     * shift = number input
+     * return -> encrypt text
+     */
+    public static String caesarEncrypt(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+        shift = shift % 26; 
+        
+        for (char c : text.toCharArray()) {
+            if (Character.isLetter(c)) {
+                char base = Character.isUpperCase(c) ? 'A' : 'a';
+                char shifted = (char) ((c - base + shift) % 26 + base);
+                result.append(shifted);
+            } else {
+                result.append(c); 
+            }
+        }
+        return result.toString();
+    }
+
+    public static String caesarDecrypt(String text, int shift){
+        return caesarEncrypt(text, -shift);
+    }
 }
