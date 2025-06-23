52ba289a826a53cf0d845223aaee23df9df8fa57
yuliia-sherstiuk <yuliia.sherstiuk@laplateforme.io>
2025-06-23 13:36:18 +0200

    Added a selection menu

--- menu.java
+++ menu.java
@@ -4,7 +4,7 @@ public class Menu {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean continueApp = true;
-
+//display menu
         while (continueApp) {
             System.out.println("======== Base Converter ========");
             System.out.println("1. Convert text to base");
@@ -12,8 +12,18 @@ public class Menu {
             System.out.println("=====================================");
             System.out.print("Your choice (1-2): ");
             String choice = scanner.nextLine();
-
-            
+//Valide menu choice
+            switch (choice) {
+                box "1": 
+                    System.out.println("1. Choose the destination base:"); 
+                    System.out.println("-h or hexadecimal - Hexadecimal"); 
+                    System.out.println("-o or octal - Octal"); 
+                    System.out.println("-d or decimal - Decimal");
+                    System.out.println("-b or binary - Binary");
+                    System.out.println("-t or text - Text");
+                    System.out.println("======================================");
+                    System.out.print("Your choice:  ");
+                    String baseChoice = scanner.nextLine().trim();
                  }
         }
     }
